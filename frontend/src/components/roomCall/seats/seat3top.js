75b88632b8a27a6f1015c60c2b8eb6fa95a3d35f
hoangnam1201 <55224036+hoangnam1201@users.noreply.github.com>
2022-08-15 21:57:44 +0700

    Fix loi

--- frontend/src/components/roomCall/seats/seat3top.js
+++ frontend/src/components/roomCall/seats/seat3top.js
@@ -1,43 +1,76 @@
-import React from 'react'
-import Avatar from 'react-avatar'
+import React from "react";
+import Avatar from "react-avatar";
 
 const Seat3top = ({ users = [] }) => {
-    return (
-        <div>
-            <div className='relative inline-block'>
-                <div className=' bg-red-200 w-96 h-4 absolute z-20 top-0' />
-                <div className=' bg-red-200 w-4 h-12 absolute z-20 -left-3 rounded-lg top-0' />
-                <div className=' bg-red-200 w-4 h-12 absolute z-20 left-96 rounded-lg top-0' />
-                <div className='grid grid-cols-3 w-96 h-20 bg-red-300 relative z-10' >
-                    <div className='flex justify-center items-center'>
-                        {users[0] && <Avatar
-                            name={users[0]?.name}
-                            size="60"
-                            round={true}
-                            className='shadow-md absolute'
-                        />}
-                    </div>
-                    <div className='border-red-400 border-l-4 flex justify-center items-center'>
-                        {users[1] && <Avatar
-                            name={users[1]?.name}
-                            size="60"
-                            round={true}
-                            className='shadow-md'
-                        />}
-                    </div>
-                    <div className='border-red-400 border-l-4 flex justify-center items-center'>
-                        {users[2] && <Avatar
-                            name={users[2]?.name}
-                            size="60"
-                            round={true}
-                            className='shadow-md'
-                        />}
-                    </div>
-                </div>
-                <div className='grid w-96 h-20 bg-red-400 absolute top-2 z-0' />
-            </div>
+  return (
+    <div>
+      <div className="relative inline-block">
+        <div className=" bg-red-200 w-96 h-4 absolute z-20 top-0" />
+        <div className=" bg-red-200 w-4 h-12 absolute z-20 -left-3 rounded-lg top-0" />
+        <div className=" bg-red-200 w-4 h-12 absolute z-20 left-96 rounded-lg top-0" />
+        <div className="grid grid-cols-3 w-96 h-20 bg-red-300 relative z-10">
+          <div className="flex justify-center items-center">
+            {users[0] &&
+              (users[0]?.picture ? (
+                <img
+                  className="absolute rounded-full shadow-lg border-gray-500 border-2"
+                  src={users[0]?.picture}
+                  alt=""
+                  width={50}
+                  height={50}
+                />
+              ) : (
+                <Avatar
+                  name={users[0]?.name}
+                  size="60"
+                  round={true}
+                  className="shadow-md absolute"
+                />
+              ))}
+          </div>
+          <div className="border-red-400 border-l-4 flex justify-center items-center">
+            {users[1] &&
+              (users[1]?.picture ? (
+                <img
+                  className="rounded-full shadow-lg border-gray-500 border-2"
+                  src={users[1]?.picture}
+                  alt=""
+                  width={50}
+                  height={50}
+                />
+              ) : (
+                <Avatar
+                  name={users[1]?.name}
+                  size="60"
+                  round={true}
+                  className="shadow-md"
+                />
+              ))}
+          </div>
+          <div className="border-red-400 border-l-4 flex justify-center items-center">
+            {users[2] &&
+              (users[2]?.picture ? (
+                <img
+                  className="rounded-full shadow-lg border-gray-500 border-2"
+                  src={users[2]?.picture}
+                  alt=""
+                  width={50}
+                  height={50}
+                />
+              ) : (
+                <Avatar
+                  name={users[2]?.name}
+                  size="60"
+                  round={true}
+                  className="shadow-md"
+                />
+              ))}
+          </div>
         </div>
-    )
-}
+        <div className="grid w-96 h-20 bg-red-400 absolute top-2 z-0" />
+      </div>
+    </div>
+  );
+};
 
-export default Seat3top
+export default Seat3top;
--- meetingBe/src/io/middlewares/auth.middleware.ts
+++ meetingBe/src/io/middlewares/auth.middleware.ts
@@ -1,19 +1,46 @@
-import dotenv from 'dotenv';
-import * as jwtService from '../../services/jwt.service';
-import { Socket } from 'socket.io';
+import dotenv from "dotenv";
+import * as jwtService from "../../services/jwt.service";
+import { Socket } from "socket.io";
+import { OAuth2Client } from "google-auth-library";
+import userModel from "../../models/user.model";
 dotenv.config();
 
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+const clientId = process.env.CLIENT_ID;
+const client = new OAuth2Client(clientId);
 
 export default class AuthMiddlesware {
-    static verifyToken = async (socket: Socket, next: any) => {
-        const token = socket.handshake.auth.token.split(' ')[1];
-        try {
-            const decoded = await jwtService.verifyToken(token, accessTokenSecret);
-            socket.data.userData = decoded;
-            next();
-        } catch (err) {
-            next(new Error('not authoried'));
+  static verifyToken = async (socket: Socket, next: any) => {
+    const token = socket.handshake.auth.token.split(" ")[1];
+    try {
+      const decoded = await jwtService.verifyToken(token, accessTokenSecret);
+      socket.data.userData = decoded;
+      next();
+    } catch (err) {
+      try {
+        const tiket = await client.verifyIdToken({
+          idToken: token,
+          audience: clientId,
+        });
+        const payload = tiket.getPayload();
+        console.log(payload);
+        const user = await userModel.findOne({ email: payload.email });
+        if (user) {
+          socket.data.userData = { userId: user._id };
+          next();
+        } else {
+          const user = new userModel({
+            name: payload.name,
+            email: payload.email,
+            picture: payload.picture,
+          });
+          await user.save();
+          socket.data.userData = { userId: user._id };
+          next();
         }
+      } catch (err) {
+        next(new Error("not authoried"));
+      }
     }
+  };
 }
